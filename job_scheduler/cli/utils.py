332e5d5e2eb9dbc118002b041a244e5bda33e407
Uriel Mandujano <uriel.mandujano14@gmail.com>
2021-06-10 22:17:56 -0500

    Unifies CLI output

--- job_scheduler/cli/utils.py
+++ job_scheduler/cli/utils.py
@@ -15,17 +15,17 @@ def get_service_addr(envvar=DEFAULT_SERVICE_ADDR_ENVVAR):
     return os.environ.get(envvar, DEFAULT_SERVICE_ADDR)
 
 
-def json_display(data: t.Dict[str, t.Any], format: t.Literal["json", "yaml"] = "yaml"):
+def terminal_display(
+    data: t.Dict[str, t.Any], format: t.Literal["json", "yaml"] = "yaml"
+):
     if format == "json":
         result = json.dumps(data, indent=2, sort_keys=True)
-        colorized = highlight(
-            result, lexers.JsonLexer(), formatters.TerminalFormatter()
-        )
+        lexer = lexers.JsonLexer()
     else:
         result = yaml.dump(data, allow_unicode=True, sort_keys=True)
-        colorized = highlight(
-            result, lexers.YamlLexer(ensurenl=True), formatters.TerminalFormatter()
-        )
+        lexer = lexers.YamlLexer(ensurenl=True)
+
+    colorized = highlight(result, lexer, formatters.TerminalFormatter())
     typer.echo(colorized, nl=False)
 
 
@@ -34,4 +34,4 @@ class OutputFormatChoices(str, enum.Enum):
     json = "json"
 
 
-OutputFormatOption = typer.Option("yaml", "--format", help="Output format")
+OutputFormatOption = typer.Option("yaml", "--output", "-o", help="Output format")
